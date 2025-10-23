047d56dd13bc4d6777a7cfda22d2d951e9e8e761
mbilaln24 <muhammad.nadeem@stud.fra-uas.de>
2025-10-23 11:08:55 +0200

    TEST WEB in Datenschutz

---
+++ helloo/src/main/java/edu/fra/uas/helloo/HellooApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.helloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellooApplication.class, args);
+	}
+
+}
